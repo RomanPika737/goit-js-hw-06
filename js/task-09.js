25fb97d6131f6e02bd93d33c4b44d549b97b3c8f
Roman <pika1roman@gmail.com>
2023-02-18 18:56:30 +0100

    updated task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,16 +1,41 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
 
-const changeColorBtn = document.querySelector('button.change-color');
-const colorSpan = document.querySelector('span.color');
-const bodyEl = document.querySelector('body');
+const refs = {
+  changeColorButton: document.querySelector("button.change-color"),
+  colorSpan: document.querySelector("span.color"),
+  bodyElem: document.querySelector("body"),
+};
 
-changeColorBtn.addEventListener('click', () => {
+function onClickChangeColor(event) {
   const newColor = getRandomHexColor();
-  bodyEl.style.backgroundColor = newColor;
-  colorSpan.textContent = newColor;
-});
+  refs.bodyElem.style.backgroundColor = newColor;
+  refs.colorSpan.textContent = newColor;
+  
+};
+
+refs.changeColorButton.addEventListener("click", onClickChangeColor);
+
+
+
+
+
+
+
+// const changeColorBtn = document.querySelector('button.change-color');
+// const colorSpan = document.querySelector('span.color');
+// const bodyEl = document.querySelector('body');
+
+// changeColorBtn.addEventListener('click', () => {
+//   const newColor = getRandomHexColor();
+//   bodyEl.style.backgroundColor = newColor;
+//   colorSpan.textContent = newColor;
+// });
+
+
+
+
 
 
 
