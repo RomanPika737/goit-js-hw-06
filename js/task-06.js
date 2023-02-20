4f29cb9896e1066aeb9a237faa09e1eda8f427d9
Roman <pika1roman@gmail.com>
2023-02-20 20:55:03 +0100

    updated task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,34 +1,42 @@
-const input = document.querySelector("#validation-input");
 
-input.addEventListener("blur", function () {
-    if (input.value.length === Number(input.dataset.length)) {
-        input.classList.remove("invalid");
-        input.classList.add("valid");
+const ref = {
+    inputEl: document.querySelector("#validation-input"),
+};
+// console.log(inputEl);
+ref.inputEl.addEventListener("blur", onInputCheckData);
+
+
+function onInputCheckData(event) {
+    if (ref.inputEl.value.length ===
+        // Number(event.currentTarget.dataset.length)
+    Number(event.currentTarget.getAttribute("data-length"))) {
+        ref.inputEl.classList.remove("invalid");
+        ref.inputEl.classList.add("valid");
     } else {
-        input.classList.remove("valid");
-        input.classList.add("invalid");
+        ref.inputEl.classList.remove("valid");
+        ref.inputEl.classList.add("invalid");
     }
-});
+};
 
 
 
 
 
 
+// const input = document.querySelector("#validation-input");
 
-// const ref = {
-//     inputEl: document.querySelector("#validation-input"),
-// };
-// // console.log(inputEl);
-// ref.inputEl.addEventListener("blur", onInputCheckData);
-
-
-// function onInputCheckData(event) {
-//     if (ref.inputEl.value.length === Number(event.currentTarget.dataset.length)) {
-//         ref.inputEl.classList.remove("invalid");
-//         ref.inputEl.classList.add("valid");
+// input.addEventListener("blur", function () {
+//     if (input.value.length === Number(input.dataset.length)) {
+//         input.classList.remove("invalid");
+//         input.classList.add("valid");
 //     } else {
-//         ref.inputEl.classList.remove("valid");
-//         ref.inputEl.classList.add("invalid");
+//         input.classList.remove("valid");
+//         input.classList.add("invalid");
 //     }
-// };
+// });
+
+
+
+
+
+
